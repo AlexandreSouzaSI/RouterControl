191bb2d0f4f6384dd7e32668970be0fafb44ad1e
Alexandre Moura de Souza <alemoura23@hotmail.com>
2026-09-08 00:29:01 -0300

    fix: imports absolutos 'src/...

--- BackEnd/backend/src/modules/caminhao/caminhao.module.ts
+++ BackEnd/backend/src/modules/caminhao/caminhao.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CaminhaoService } from './caminhao.service';
 import { CaminhaoController } from './caminhao.controller';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
     imports: [PrismaModule],
--- BackEnd/backend/src/relatorio/relatorio.module.ts
+++ BackEnd/backend/src/relatorio/relatorio.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { RelatorioController } from './relatorio.controller';
 import { RelatorioService } from './relatorio.service';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [RelatorioController],
